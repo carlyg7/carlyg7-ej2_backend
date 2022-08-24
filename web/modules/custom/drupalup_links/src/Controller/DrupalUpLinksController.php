c08ff3c0ec92704150c0e0ce362750872a77002e
carlyg7 <carlosgm46@gmail.com>
2022-08-24 11:32:12 +0200

    creacion modulo links

---
+++ web/modules/custom/drupalup_links/src/Controller/DrupalUpLinksController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\drupalup_links\Controller;
+
+class DrupalUpLinksController {
+
+  public function enlace_pagina() {
+    return [
+      '#markup' => 'Pagina de enlace principal.',
+    ];
+  }
+
+
+}
